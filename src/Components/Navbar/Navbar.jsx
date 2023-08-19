1d7152e6bf86d51b25b1500c94ed62cc71e4f6ba
kazim786110 <kazimnaqvi10@gmail.com>
2023-08-19 17:22:29 +0500

    functions

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,24 +2,22 @@ import React from 'react'
 import '../Navbar/Navbar.css'
 
 const Navbar = () => {
+    const Mustafa = "Kazim"
+    const handleOnclick = ()=>{console.log("hello WOkrd")}
+    const Suri = ()=>{console.log("hello Suri")}
+    
+
   return (
 
-    <div>
-
-        <div className='logo'>
-            logo
-            <div>
-        
-            </div>
-            <div>
-
-            </div>
-        </div>
-        <div>
-
-        </div>
-
-    </div>
+<nav className="navbar">
+        <div className="navbar-logo">Logo</div>
+        <ul className="navbar-list">
+          <li   onClick={handleOnclick} className="navbar-item">Home</li>
+          <li  onClick={Suri} className="navbar-item">About</li>
+          <li className="navbar-item">Services</li>
+          <li className="navbar-item">Contact</li>
+        </ul>
+      </nav>
         
   )
 }
